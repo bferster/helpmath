ef619176393311f2bc17b68d823cff61f687c230
bferster <bferster@stagetools.com>
2023-12-14 16:47:29 -0500

    updated ui

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ class App  {
 		this.lesson=0;																				// Current lesson
 		this.topic=0;																				// Current topic
 		this.page=0;																				// Current page
+		this.id="";																					// Position id
 		this.course={name: "HelpMath", version:.9, modules:[]};										// Course data
 		this.LoadConfig("project.json");															// Load config file and set up project
 		this.language="English";																	// Language
+		this.topics=["Your world","Learn it!","Play it!","Try it!","Practice test","Quiz"];			// Topic names
 	}
 
 	LogIn(name, password)																		// LOG IN
@@ -39,6 +41,7 @@ class App  {
 			if (!i) n=8;
 			for (j=0;j<n;++j) {																		// For each lesson
 				this.course.modules[i].lessons.push({ name: "Lesson "+(j+1), id:"0"+((i+1)*1000000+(j+1)*10000), topics:[] });				
+				this.course.modules[i].lessons[j].title="Lesson Title";
 				for (k=0;k<7;++k) {																	// For each topic
 					this.course.modules[i].lessons[j].topics.push({ name: "Topic "+(k+1), id:"0"+((i+1)*1000000+(j+1)*10000+(k+1)*100), pages:[] }); 		
 					for (l=0;l<Math.random()*8+4;++l) {												// For each page
@@ -46,7 +49,8 @@ class App  {
 						}
 					}
 				}
-		o=this.course.modules[0].lessons;														
+		o=this.course.modules[0].lessons;
+		o[0].title="Counting on Numbers";														
 		o[0].name="Place Value";	o[1].name="Addition and Subtraction";	
 		o[2].name="Multiplication";	o[3].name="Division";	
 		o[4].name="Fractions";		o[5].name="Decinmals and Money";	
@@ -58,8 +62,8 @@ class App  {
 	{
 		let o,i;
 		let str=`<div class="hm-module">
-			<img src="img/logo.png" style="margin:48px 0 24px 0;width:20%"><br>
-			<span style="font-size:2vw;font-weight:500">Choose your course</span><br><br>`;			// Title
+		<img src="img/logo.png" style="margin:48px 0 24px 0;width:20%"><br>
+		<span style="font-size:2vw;font-weight:500">Choose your course</span><br><br>`;			// Title
 		for (i=0;i<this.course.modules.length;++i) 													// For each module in course	
 			str+=`<div class="hm-modbut" style="background-color:#${ module == (i+1) ? "009900" : "#85b9d"}" id="hm-module-${i}">${trans(this.course.modules[i].name)}</div>`;	// Add to menu
 		if (module != 0) {
@@ -71,7 +75,6 @@ class App  {
 			str+="</div";																			// Close lesson div																
 			}
 		str+="</div>";																				// Close module div
-		$("#hm-main").css("display","block");														// Remove grid styling
 		$("#hm-main").html(str.replace(/\t|\n|\r/g,""));											// Add markup
 		$("#hm-lesson-"+app.lesson).css("background-color","#a7cea7");
 	
@@ -92,48 +95,56 @@ class App  {
 	ShowLesson()																				// SHOW LESSON
 	{
 		let les=app.course.modules[app.module].lessons[app.lesson];									// Point at lesson
-		$("#hm-main").html(`<div id="hm-left" class="hm-left"></div><div id="hm-right" class="hm-right"></div>`);
-		let str=`<img src="img/logo.png" style="width:100px"><br><br>
+/*		let str=`<img src="img/logo.png" style="width:3vw"><br><br>
 				<img id="hm-topicTri" src="img/triangle.png" style="position:absolute;left:36px">
-				<div id="hm-topic-0" class="hm-topicbut">${trans("Your world")}</div>
-				<div id="hm-topic-1" class="hm-topicbut">${trans("Learn it!")}</div>
-				<div id="hm-topic-2" class="hm-topicbut">${trans("Play it!")}</div>
-				<div id="hm-topic-3" class="hm-topicbut">${trans("Important words!")}</div>
-				<div id="hm-topic-4" class="hm-topicbut">${trans("Try it!")}</div>
-				<div id="hm-topic-5" class="hm-topicbut">${trans("Practice test")}</div>
-				<div id="hm-topic-6" class="hm-topicbut">${trans("Final quiz")}</div>
-				<div id="hm-done" class="hm-topicbut">${trans("Done")}</div>`;
-			$("#hm-left").html(str.replace(/\t|\n|\r/g,""));										// Add left side markup
+		$("#hm-left").html(str.replace(/\t|\n|\r/g,""));										// Add left side markup
+*/
+			let str=`<img src="img/logo2.png" style="float:left;height:5vw;margin:16px 0 0 16px">
+			<img id="hm-close" src="img/closebut.png" title="Close / Cerrar"style="width:20px;float:right;margin:16px 16px 0 0;cursor:pointer">
+	
+			<div style="text-align:center;width:calc(16% - 32px);position:absolute;left:32px;top:20vw">
+				<div id="hm-map" class="hm-topicbut">${trans("Map")}</div>
+				<div id="hm-keyterms" class="hm-topicbut">${trans("Keyterms")}</div>
+				<div id="hm-formulas" class="hm-topicbut">${trans("Formulas")}</div>
+				<div id="hm-spanish" class="hm-topicbut"  title="Hear in Spanish">En esta paginá</div>
+			</div>
+			
+			<div style="text-align:center;width:84%;padding-top:12px;margin-bottom:16px;">
+				<div class="hm-lessonTitle"<div>${trans(les.title)}</div>
+				<div class="hm-lessonSubTitle">${trans(les.name)}: ${trans(app.topics[app.topic])}</div>
+			</div>
+			<div id="hm-screen" class="hm-screen">
+				<div id="hm-video" class="hm-video"><img src="video.png" style="width:100%"></div>
+				<img id="hm-next" src="img/next.png" class="hm-next" title="Next / proxima">
 
-			str=`<img src="video.png" style="width:100%;border-radius:12px;max-height:calc(100vh - 180px); >
-			<div id="hm-playerControl">
+				<div id="hm-playerControl">
 					<div id="hm-timeSlider" class="hm-timeSlider"></div>
 					<img id="hm-play" src="img/playbut.png"  title="Play lesson" style="vertical-align:-8px">
-					<div id="hm-keyterms" class="hm-topicbut" style="position:absolute;left:268px;top:calc(100vh - 80px)">${trans("Keyterms")}</div>
-					<div id="hm-formulas" class="hm-topicbut" style="position:absolute;left:390px;top:calc(100vh - 80px)">${trans("Formulas")}</div>
-					<img src="img/blilogo.png"style="width:175px;position:absolute;left:32px;bottom:32px">
-					<div id="hm-audioSlider" title="Adjust volume" class="hm-audioSlider"></div>
-					<img src="img/speaker.png" title="Adjust volume" style="position:absolute; left:calc(100% - 185px); top:calc(100vh - 60px)">
-					<img src="img/language.png" title="Set language" style="position:absolute; left:calc(100% - 230px); top:calc(100vh - 60px)">
-					<img src="img/helpicon.png" title="Get help" style="position:absolute; left:calc(100% - 275px); top:calc(100vh - 60px)">
-					<img src="img/calculator.png" title="Use calculator" style="position:absolute; left:calc(100% - 318px); top:calc(100vh - 60px)">
-					<img src="img/notepad.png" title="Take notes" style="position:absolute; left:calc(100% - 360px); top:calc(100vh - 60px)">
-					<div class="hm-lessonTitle">${trans(les.name)}</div>
-					<div id="hm-pageback" style="width:60%;margin-left:20%;height:24px"></div>`;
-					$("#hm-right").html(str.replace(/\t|\n|\r/g,""));											// Add right side markup
-
+				</div>
+		<div id="hm-pages" style="margin-top:8px;width:100%;text-align:center"></div>
+		</div>`
+		$("#hm-main").html(str.replace(/\t|\n|\r/g,""));											// Add  markup
 		this.DrawPageBar(les.topics[app.topic].pages);												// Draw page bar
-		$("#hm-main").css("display","grid");
+		str=`<div id="hm-appControls" style="position:absolute;left:30px;top:calc(100vh - 64px);width:calc(100% - 80px)">
+			<div style="float:right;margin-top:4px">
+				<img src="img/notepad.png" title="Take notes"class="hm-iconButton">
+				<img src="img/calculator.png" title="Use calculator" class="hm-iconButton">
+				<div id="hm-audioSlider" title="Adjust volume" class="hm-audioSlider"></div>
+				<img src="img/helpicon.png" title="Get help" class="hm-iconButton"">
+			</div>
+		</div>`;
+		$("#hm-main").append(str.replace(/\t|\n|\r/g,""));											// Add right side markup
+		$("#hm-main").css("display","block");
 		$("#hm-timeSlider").slider({});																// Init timeslider
-		$("#hm-audioSlider").slider({ value:50});													// Init audioslider
+		$("#hm-audioSlider").slider({ value:50 });													// Init audioslider
 		$("#hm-topic-"+app.topic).css("background-color","#009900");								// Higlight current topic
 		$("#hm-topicTri").css("top",app.topic*41+184+"px");											// Triangle
-	
+
 		$("#hm-play").on("click",()=>{
 			$("#hm-play").prop("src",$("#hm-play").prop("src").match(/play/i) ? "img/pausebut.png" : "img/playbut.png");
 			});
 	
-		$("#hm-done").on("click",()=>{
+		$("#hm-close").on("click",()=>{
 			app.ShowModules(app.module);															// PICK A MODULE
 			});
 		
@@ -154,7 +165,7 @@ class App  {
 		for (i=0;i<pages.length;++i)																// For each page
 			str+=`<div id="hm-page-${i}" class="hm-page" style="width:calc(${pct}% - ${slop}px)" - >${i+1}</div>`;
 		str+=`<div id="hm-pageNext" class="hm-page" style="background-color:#185b9d;cursor:pointer;width:16px;border-radius:0 12px 12px 0">></div>`
-		$("#hm-pageback").html(str.replace(/\t|\n|\r/g,""));										// Add markup
+		$("#hm-pages").html(str.replace(/\t|\n|\r/g,""));											// Add markup
 		
 		$("#hm-page-0").css("background-color","#6cbe6f");
 		$("#hm-page-"+app.page).css("background-color","#009900");
